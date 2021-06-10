eb3a44ca51d5d1214d23d08f350dbad24171c0f9
Jorge Tinoco <jtinoco@zenput.com>
2021-06-10 18:06:56 -0500

    Add priority and due_date to TODO model

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,12 +1,25 @@
+from datetime import datetime
+
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from enum import Enum
 
 # Create your models here.
+
+
+class Priority(models.TextChoices):
+    HIGH = "High", _("High")
+    MEDIUM = "Med", _("Medium")
+    LOW = "Low", _("Low")
 
 
 class Todo(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
+    priority = models.TextField(choices=Priority.choices, default=Priority.LOW)
+    due_date = models.DateTimeField(auto_now_add=True, blank=True)
 
     def __str__(self):
         return self.title
