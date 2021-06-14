d05a31e98ebc3802a54355dd79c704a7fe50a76a
Jorge Tinoco <jtinoco@zenput.com>
2021-06-14 11:06:28 -0500

    Add pagination in the API

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-from rest_framework import viewsets, generics, filters
+from rest_framework import viewsets, filters
+from rest_framework.pagination import PageNumberPagination
+
 from .serializers import TodoSerializer
 from .models import Todo
 
 # Create your views here
+
+
+class Pagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = "page_size"
+    max_page_size = 10
 
 
 class TodoView(viewsets.ModelViewSet):
@@ -12,3 +20,4 @@ class TodoView(viewsets.ModelViewSet):
     search_fields = ["title"]
     ordering_fields = ["title", "priority", "due_date"]
     filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    pagination_class = Pagination
